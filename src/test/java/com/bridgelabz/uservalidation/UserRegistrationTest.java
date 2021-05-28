caae020b06d39e9c56c7be73324c8c6879298e8b
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-28 17:52:25 +0530

    ADDED: create class

---
+++ src/test/java/com/bridgelabz/uservalidation/UserRegistrationTest.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.uservalidation;
+
+import org.junit.Test;
+
+public class UserRegistrationTest {
+    @Test
+    public void giveFirstName_WhenProper_returnTrue() {
+        new UserRegistration();
+    }
+}
