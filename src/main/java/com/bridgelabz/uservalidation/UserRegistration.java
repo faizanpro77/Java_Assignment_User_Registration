4170d07fb1d32a5efd3713dc0eabc4741924d4bb
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-28 19:24:30 +0530

    MODIFIED: add fuctionality to fistName validation

--- src/main/java/com/bridgelabz/uservalidation/UserRegistration.java
+++ src/main/java/com/bridgelabz/uservalidation/UserRegistration.java
@@ -1,5 +1,13 @@
 package com.bridgelabz.uservalidation;
 
-public class UserRegistration {
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+    private static final String FIRST_NAME_PATTERN = "^[A-Z][a-z]{3,}";
+    public boolean validateFirstName(String firstName) {
+        Pattern pattern = Pattern.compile(FIRST_NAME_PATTERN);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.find();
+    }
 }
--- src/test/java/com/bridgelabz/uservalidation/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/uservalidation/UserRegistrationTest.java
@@ -1,10 +1,27 @@
 package com.bridgelabz.uservalidation;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void giveFirstName_WhenProper_returnTrue() {
-        new UserRegistration();
+    public void givenFirstName_whenProper_returnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean status = userRegistration.validateFirstName("Fakk");
+        Assert.assertEquals(true, status);
+    }
+
+    @Test
+    public void givenFirstName_whenShort_returnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean status = userRegistration.validateFirstName("Fa");
+        Assert.assertEquals(false, status);
+    }
+
+    @Test
+    public void givenFirst_whenInvalid_returnFalse() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean status = userRegistration.validateFirstName("faizan");
+        Assert.assertEquals(false, status);
     }
 }
