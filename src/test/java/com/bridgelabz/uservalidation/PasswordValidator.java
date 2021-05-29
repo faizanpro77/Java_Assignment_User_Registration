cddf26d072249c419f96f0753f3bf868c5dbddb8
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-29 23:11:37 +0530

    MODIFIED : add functionality to validate password with atleat 1 upper case

--- src/test/java/com/bridgelabz/uservalidation/PasswordValidator.java
+++ src/test/java/com/bridgelabz/uservalidation/PasswordValidator.java
@@ -8,7 +8,14 @@ public class PasswordValidator {
     @Test
     public void givenPassword_whenMinimum8Characters_returnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean status = userRegistration.validatePassword("faiz@a78");
+        boolean status = userRegistration.validatePassword("faiZ@a78");
+        Assert.assertEquals(true, status);
+    }
+
+    @Test
+    public void givenPassword_when1UpperCase_returnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean status = userRegistration.validatePassword("faIz@a78");
         Assert.assertEquals(true, status);
     }
 }
