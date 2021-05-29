7d8d47215bb0260d5ec2dddaa67157f6a1e70bdb
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-29 23:59:09 +0530

    MODIFIED: add functionality to validate password atleast one numeric

--- src/test/java/com/bridgelabz/uservalidation/PasswordValidator.java
+++ src/test/java/com/bridgelabz/uservalidation/PasswordValidator.java
@@ -8,15 +8,22 @@ public class PasswordValidator {
     @Test
     public void givenPassword_whenMinimum8Characters_returnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean status = userRegistration.validatePassword("faiZ@a78");
+        boolean status = userRegistration.validatePassword("faiZ@178");
         Assert.assertEquals(true, status);
     }
 
     @Test
     public void givenPassword_when1UpperCase_returnTrue() {
         UserRegistration userRegistration = new UserRegistration();
-        boolean status = userRegistration.validatePassword("faIz@a78");
+        boolean status = userRegistration.validatePassword("faIZ@278");
         Assert.assertEquals(true, status);
+    }
+
+    @Test
+    public void givenPassword_when1Numeric_returnTrue() {
+        UserRegistration userRegistration = new UserRegistration();
+        boolean status = userRegistration.validatePassword("faIz@378");
+        Assert.assertEquals(true,  status);
     }
 }
 
