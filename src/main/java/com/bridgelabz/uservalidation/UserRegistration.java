6eb157c111b58f5a8fad754004607b884a3f9f8d
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-29 17:18:20 +0530

    MODIFIED: add functionality to email validation

--- src/main/java/com/bridgelabz/uservalidation/UserRegistration.java
+++ src/main/java/com/bridgelabz/uservalidation/UserRegistration.java
@@ -5,13 +5,18 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
     private static final String NAME_PATTERN = "^[A-Z][a-z]{3,}";
+    private static final String EMAIL_PATTERN = "^[0-9A-Za-z]+([-_+.][0-9A-Za-z]+)?[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?$";
 
     public boolean validateFirstName(String firstName) {
-       return patternChecker(firstName, NAME_PATTERN);
+        return patternChecker(firstName, NAME_PATTERN);
     }
 
     public boolean validateLastName(String lastName) {
        return patternChecker(lastName, NAME_PATTERN);
+    }
+
+    public boolean validateEmail(String email) {
+        return patternChecker(email, EMAIL_PATTERN);
     }
 
     private static boolean patternChecker(String input, String inputPattern) {
--- src/test/java/com/bridgelabz/uservalidation/EmailValidator.java
+++ src/test/java/com/bridgelabz/uservalidation/EmailValidator.java
@@ -1,2 +1,34 @@
-package com.bridgelabz.uservalidation;public class EmailValidator {
+package com.bridgelabz.uservalidation;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class EmailValidator {
+    public String email;
+    public boolean status;
+    UserRegistration userRegistration = new UserRegistration();
+    public EmailValidator(String email, boolean status) {
+        this.email = email;
+        this.status = status;
+    }
+    @Parameterized.Parameters
+    public static Collection input() {
+        return Arrays.asList(new Object[][]{{"abc@yahoo.com", true}, {"abc-100@yahoo.com", true}, {"abc.100@yahoo.com", true},
+                {"abc111@abc.com", true}, {"abc-100@abc.net", true}, {"abc.100@abc.com.au", true}, {"abc@1.com", true},
+                {"abc+100@gmail.com", true}, {"invalid123@pole.com", true}, {"abc@.com.my", false}, {"abc123@gmail.a", false},
+                {"abc123@.com", false}, {"abc123@.com.com", false} });
+    }
+
+    @Test
+    public void emailChecker() {
+        System.out.println("email ids -->" + this.email);
+        boolean result = userRegistration.validateEmail(email);
+        Assert.assertEquals(this.status, result);
+    }
 }
