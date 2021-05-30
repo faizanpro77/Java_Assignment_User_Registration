9aa6ff60df13203b4c293bbe1f6174b284c0ba0c
faizanpro77 <faizyshaikh204@gmail.com>
2021-05-30 10:58:06 +0530

    MODIFIED: check email sample

--- src/test/java/com/bridgelabz/uservalidation/EmailValidator.java
+++ src/test/java/com/bridgelabz/uservalidation/EmailValidator.java
@@ -22,7 +22,7 @@ public class EmailValidator {
         return Arrays.asList(new Object[][]{{"abc@yahoo.com", true}, {"abc-100@yahoo.com", true}, {"abc.100@yahoo.com", true},
                 {"abc111@abc.com", true}, {"abc-100@abc.net", true}, {"abc.100@abc.com.au", true}, {"abc@1.com", true},
                 {"abc+100@gmail.com", true}, {"invalid123@pole.com", true}, {"abc@.com.my", false}, {"abc123@gmail.a", false},
-                {"abc123@.com", false}, {"abc123@.com.com", false} });
+                {"abc123@.com", false}, {"abc123@.com.com", false}, {"abc()*@gmail.com", false} });
     }
 
     @Test
